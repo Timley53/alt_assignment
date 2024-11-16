7b53762924758d5097451241f70c806b3fbbf8f6
timley53 <adedokuntimileyin123@gmail.com>
2024-11-16 17:21:50 +0100

    assignment done

---
+++ myDb.py
@@ -0,0 +1,10 @@
+user_Db = {
+    "user0": {
+        "first_name": "rajesh",
+        "last_name": "koothrapaali",
+        "email": "raj13@example.com",
+        "age": 23,
+        "height": 3.6,
+    }
+}
+
